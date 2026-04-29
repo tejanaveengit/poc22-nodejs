99c7cd325111124179cf21bdbfabd00d21beaa7a
tejanaveengit <peesatejanaveen@gmail.com>
2026-04-29 16:18:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("✅ Hello from CI/CD deployed Node.js application POC 22!");
+  res.send("✅ Hello from CI/CD deployed Node.js application using Github Actions POC 22!");
 });
 
 app.get("/health", (req, res) => {
