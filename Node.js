6d9709b49b0a97fb22b963d8564f55544a5abae9
tejanaveengit <peesatejanaveen@gmail.com>
2026-04-29 11:50:59 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,1 +1,16 @@
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("✅ Hello from CI/CD deployed Node.js app!");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK", time: new Date() });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
